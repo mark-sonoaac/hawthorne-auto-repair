a6b19a6da33e334eb680b35bd56e20231e9a3be0
mark-sonoaac <mark@sonoaac.com>
2026-01-10 15:32:05 -0500

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/hawthorne-auto-repair/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
